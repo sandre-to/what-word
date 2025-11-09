4b40656771ef9b83693b34020dc2ebf92890b370
sandre-to <220078277+sandre-to@users.noreply.github.com>
2025-11-09 13:20:36 +0100

    Read from file

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -3,7 +3,17 @@ class_name GameScene extends Control
 @onready var letter_container: HBoxContainer = $WordsContainer/LetterContainer
 @onready var check_button: Button = $CheckButton
 
-var guessed_word: Array[String] = []
+var current_word: String = ""
+var guessed_word_sequence: Array[String] = []
+var word_file_path: String = "res://assets/list_of_words/5-letter-words.txt"
+var rng := RandomNumberGenerator.new()
+
+func _ready() -> void:
+	var words := load_from_file(word_file_path)
+	var i = rng.randi_range(0, words.size())
+	var get_random_word = words.get(i)
+	current_word = get_random_word
+	print(current_word)
 
 func _on_check_button_pressed() -> void:
 	for letter: LineEdit in letter_container.get_children():
@@ -11,5 +21,16 @@ func _on_check_button_pressed() -> void:
 		if letter.text == "": return
 		
 		letter.editable = false
-		guessed_word.append(letter.text)
-	print(guessed_word)
+		guessed_word_sequence.append(letter.text)
+	
+	var guessed_word: String = "".join(guessed_word_sequence).to_lower()
+	if guessed_word == current_word:
+		print("U GJUESSED RIGHT")
+	else:
+		print("WRONG!")
+
+func load_from_file(path) -> PackedStringArray:
+	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+	var content: String = file.get_as_text()
+	var words_list: PackedStringArray = content.split("\n")
+	return words_list
